d5788e9036b93376abf0c0e5bb2a2e1c0d2927fe
tudoriordache2004 <tudor-dimitrie.iordache@s.unibuc.ro>
2025-05-04 19:28:14 +0300

    Added destructibles

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    [SerializeField] private GameObject destroyVFX;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<DamageSource>())
+        {
+            Instantiate(destroyVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+}
