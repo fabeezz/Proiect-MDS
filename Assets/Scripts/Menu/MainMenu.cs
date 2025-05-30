b06db96a529ad6a66de248e5b7416ae70c933df4
tudoriordache2004 <tudor-dimitrie.iordache@s.unibuc.ro>
2025-05-30 22:18:25 +0300

    Initialized the main menu script

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene1"); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game"); 
+    }
+}
