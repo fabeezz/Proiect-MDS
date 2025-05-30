860c44a5fa03f48a7c32743bb0ad01462ab01300
andra2602 <andra.andruta60@yahoo.com>
2025-05-30 15:01:51 +0300

    Code Review - Player

--- Assets/Scripts/Player/SlashAnim.cs
+++ Assets/Scripts/Player/SlashAnim.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Controls the lifecycle of a slash particle animation.
+/// Destroys the GameObject once the particle system finishes playing.
+/// </summary>
 public class SlashAnim : MonoBehaviour
 {
     private ParticleSystem ps;
 
+    /// <summary>
+    /// Gets the ParticleSystem component attached to this GameObject.
+    /// </summary>
     private void Awake()
     {
         ps = GetComponent<ParticleSystem>();
     }
+    /// <summary>
+    /// Checks each frame if the particle effect is finished,
+    /// and destroys the object if it is no longer alive.
+    /// </summary>
     private void Update()
     {
         if(ps && !ps.IsAlive())
@@ -17,6 +28,9 @@ public class SlashAnim : MonoBehaviour
             DestroySelf();
         }
     }
+    /// <summary>
+    /// Safely destroys the slash effect GameObject.
+    /// </summary>
     public void DestroySelf()
     {
         Destroy(gameObject);
